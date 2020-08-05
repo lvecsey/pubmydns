3cf872e37397dd77768d3657eabcb16020ff9b23
Lester Vecsey <lvecsey@gmail.com>
2020-08-05 04:37:41 +0000

    update license interface_ip code

--- interface_ip.h
+++ interface_ip.h
@@ -1,3 +1,21 @@
+/*
+    Extract the IP address from the specified interface using ifconfig
+    Copyright (C) 2020  Lester Vecsey
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #ifndef INTERFACE_IP_H
 #define INTERFACE_IP_H
 
